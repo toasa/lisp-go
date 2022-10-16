cf5a18d3879e635543fcbc2bc6d0922d3a0f9193
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-16 16:01:16 +0900

    Add token package

---
+++ token/token.go
@@ -0,0 +1,61 @@
+package token
+
+import (
+	"strconv"
+	"strings"
+)
+
+type TokenKind int
+
+const (
+	Int TokenKind = iota
+	Symbol
+	LParen
+	RParen
+)
+
+type Token struct {
+	Kind   TokenKind
+	Val    int    // If kind is Int, its value
+	Symbol string // If kind is Symbol, its value
+}
+
+func IntToken(n int) Token {
+	return Token{Kind: Int, Val: n}
+}
+
+func LParenToken() Token {
+	return Token{Kind: LParen}
+}
+
+func RParenToken() Token {
+	return Token{Kind: RParen}
+}
+
+func SymbolToken(s string) Token {
+	return Token{Kind: Symbol, Symbol: s}
+}
+
+func Tokenize(program string) []Token {
+	program = strings.ReplaceAll(program, "(", " ( ")
+	program = strings.ReplaceAll(program, ")", " ) ")
+	words := strings.Fields(program)
+
+	tokens := []Token{}
+	for _, word := range words {
+		switch word {
+		case "(":
+			tokens = append(tokens, LParenToken())
+		case ")":
+			tokens = append(tokens, RParenToken())
+		default:
+			if n, err := strconv.Atoi(word); err == nil {
+				tokens = append(tokens, IntToken(n))
+			} else {
+				tokens = append(tokens, SymbolToken(word))
+			}
+		}
+	}
+
+	return tokens
+}
---
+++ token/token_test.go
@@ -0,0 +1,78 @@
+package token
+
+import (
+	"testing"
+)
+
+type test struct {
+	input    string
+	expected []Token
+}
+
+func TestTokenize(t *testing.T) {
+	tests := []test{
+		{
+			input: "(+ 1 2)",
+			expected: []Token{
+				LParenToken(),
+				SymbolToken("+"),
+				IntToken(1),
+				IntToken(2),
+				RParenToken(),
+			},
+		},
+		{
+			input: `(
+                (define r 10)
+                (define pi 314)
+                (* pi (* r r))
+            )`,
+			expected: []Token{
+				LParenToken(),
+				LParenToken(),
+				SymbolToken("define"),
+				SymbolToken("r"),
+				IntToken(10),
+				RParenToken(),
+				LParenToken(),
+				SymbolToken("define"),
+				SymbolToken("pi"),
+				IntToken(314),
+				RParenToken(),
+				LParenToken(),
+				SymbolToken("*"),
+				SymbolToken("pi"),
+				LParenToken(),
+				SymbolToken("*"),
+				SymbolToken("r"),
+				SymbolToken("r"),
+				RParenToken(),
+				RParenToken(),
+				RParenToken(),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tokens := Tokenize(test.input)
+		if len(tokens) != len(test.expected) {
+			t.Errorf("Token length mismatch (Actual: %d, Expected: %d)", len(tokens), len(test.expected))
+		}
+
+		for i, token := range tokens {
+			expected := test.expected[i]
+			if token.Kind != expected.Kind {
+				t.Errorf("Token kind mismatch (Actual: %d, Expected: %d)",
+					token.Kind, expected.Kind)
+			}
+			if token.Kind == Int && token.Val != expected.Val {
+				t.Errorf("Int token value mismatch (Actual: %d, Expected: %d)",
+					token.Val, expected.Val)
+			}
+			if token.Kind == Symbol && token.Symbol != expected.Symbol {
+				t.Errorf("Symbol token value mismatch (Actual: %s, Expected: %s)",
+					token.Symbol, expected.Symbol)
+			}
+		}
+	}
+}
