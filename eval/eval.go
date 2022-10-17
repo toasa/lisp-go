ee7581c4c0fda7a3ba9a1615069a942af797b741
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-17 22:12:33 +0900

    Add eval package

---
+++ eval/eval.go
@@ -0,0 +1,73 @@
+package eval
+
+import (
+	"fmt"
+	. "lisp-go/object"
+)
+
+func Eval(obj Object) (Object, error) {
+	return evalObj(obj)
+}
+
+func evalObj(obj Object) (Object, error) {
+	switch obj.Kind {
+	case Int:
+		return IntObject(obj.Val), nil
+	case List:
+		return evalList(obj)
+	}
+	return Object{}, fmt.Errorf("Invalid Object: %s", obj)
+}
+
+func evalList(list Object) (Object, error) {
+	head := list.List[0]
+
+	switch head.Kind {
+	case Symbol:
+		switch head.Symbol {
+		case "+":
+			return evalBinaryOp(list)
+		}
+	default:
+		return Object{}, fmt.Errorf("Unsupport list %s", list)
+	}
+
+	new_list := []Object{}
+	for _, elem := range list.List {
+		res, _ := evalObj(elem)
+		new_list = append(new_list, res)
+	}
+	return ListObject(new_list), nil
+}
+
+func evalBinaryOp(list Object) (Object, error) {
+	if len(list.List) != 3 {
+		return Object{}, fmt.Errorf("Invalid number of arguments for infix operator")
+	}
+
+	op := list.List[0]
+	lhs, _ := evalObj(list.List[1])
+	rhs, _ := evalObj(list.List[2])
+
+	if lhs.Kind != Int {
+		return Object{}, fmt.Errorf("Left operand must be an integer (%s)", lhs)
+	}
+	if rhs.Kind != Int {
+		return Object{}, fmt.Errorf("Right operand must be an integer (%s)", rhs)
+	}
+
+	lval := lhs.Val
+	rval := rhs.Val
+
+	switch op.Kind {
+	case Symbol:
+		switch op.Symbol {
+		case "+":
+			return IntObject(lval + rval), nil
+		default:
+			return Object{}, fmt.Errorf("%s unsupported", op.Symbol)
+		}
+	default:
+		return Object{}, fmt.Errorf("Operator must be a symbol")
+	}
+}
---
+++ eval/eval_test.go
@@ -0,0 +1,51 @@
+package eval
+
+import (
+	. "lisp-go/object"
+	"testing"
+)
+
+type test struct {
+	input    Object
+	expected Object
+}
+
+func TestEval(t *testing.T) {
+	tests := []test{
+		{
+			input: ListObject(
+				[]Object{
+					SymbolObject("+"),
+					IntObject(10),
+					IntObject(20),
+				}),
+			expected: IntObject(30),
+		},
+		{
+			input: ListObject(
+				[]Object{
+					SymbolObject("+"),
+					ListObject(
+						[]Object{
+							SymbolObject("+"),
+							IntObject(3),
+							IntObject(4),
+						}),
+					IntObject(5),
+				}),
+			expected: IntObject(12),
+		},
+	}
+
+	for _, test := range tests {
+		list, err := Eval(test.input)
+		if err != nil {
+			t.Errorf("Eval failed: %s", err)
+		}
+
+		expected := test.expected
+		if !Equal(list, expected) {
+			t.Errorf("Object mismatch (Actual: %s, Expected: %s)", list, expected)
+		}
+	}
+}
