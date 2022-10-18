954e1c31dda5ce7ed1970c9e4ee964d5825cf53b
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-18 18:56:00 +0900

    Chore
    fmt

--- parse/parse.go
+++ parse/parse.go
@@ -15,25 +15,24 @@ func Parse(tokens []token.Token) (Object, error) {
 var tmp []token.Token
 
 func parseList(tokens []token.Token) (Object, error) {
-
-	t := tokens[0]
-	if t.Kind != token.LParen {
-		return None, fmt.Errorf("Expected LParen, got %s", t)
+	head := tokens[0]
+	if head.Kind != token.LParen {
+		return None, fmt.Errorf("Expected LParen, got %s", head)
 	}
 	tokens = tokens[1:]
 
 	list := []Object{}
 	for len(tokens) > 0 {
-		t = tokens[0]
+		head = tokens[0]
 		tokens = tokens[1:]
 
-		switch t.Kind {
+		switch head.Kind {
 		case token.Int:
-			list = append(list, IntObject(t.Val))
+			list = append(list, IntObject(head.Val))
 		case token.Symbol:
-			list = append(list, SymbolObject(t.Symbol))
+			list = append(list, SymbolObject(head.Symbol))
 		case token.LParen:
-			tokens = append([]token.Token{t}, tokens...)
+			tokens = append([]token.Token{head}, tokens...)
 			sub_list, _ := parseList(tokens)
 			list = append(list, sub_list)
 			tokens = tmp
@@ -41,7 +40,7 @@ func parseList(tokens []token.Token) (Object, error) {
 			tmp = tokens
 			return ListObject(list), nil
 		default:
-			return None, fmt.Errorf("Invalid token %s", t)
+			return None, fmt.Errorf("Invalid token %s", head)
 		}
 	}
 
